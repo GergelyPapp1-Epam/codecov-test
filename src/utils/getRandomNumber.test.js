b94563ae4a455d8ea1ce019f52af8a97e6a03c6b
Gergely Papp1 <gergely_papp1@ephubudw00e0.home>
2024-07-17 13:25:11 +0200

    anotha test

--- src/utils/getRandomNumber.test.js
+++ src/utils/getRandomNumber.test.js
@@ -1,8 +1,16 @@
 import { getRandomNumber } from "./getRandomNumber";
 
 
-it("returns a random number that's lower than max number ", () => {
+it("returns a random number that's lower than 100 ", () => {
     const maxNum = 100;
+    const randomNumber = getRandomNumber(maxNum);
+
+    expect(randomNumber < maxNum).toBeTruthy()
+})
+
+
+it("returns a random number that's lower than 10 ", () => {
+    const maxNum = 10;
     const randomNumber = getRandomNumber(maxNum);
 
     expect(randomNumber < maxNum).toBeTruthy()
